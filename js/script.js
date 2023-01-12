7bdb4dcb02d57580d73d8fc89e7e388bc5137059
XalqlarDoner <122517996+XalqlarDoner@users.noreply.github.com>
2023-01-12 11:51:51 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const adress = document.querySelector('#adress');
+const menu = document.querySelector('#menu');
+menu.onclik = () =>{
+    console.log("hello");
+}
+adress.onclik = () =>{
+    console.log("hello");
+}
